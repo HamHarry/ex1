50cbbaf6b46aa3f88eb7276d424619f20f581a9e
HamHarry <hamza_2641@hotmail.com>
2024-07-25 20:14:05 +0700

    ยังไม่เสร็จ ทำคืนนี้ต่อ

--- src/views/Test4.jsx
+++ src/views/Test4.jsx
@@ -1,71 +1,204 @@
+import { useState } from "react";
+import mockup from "../Mockup";
+
 /* eslint-disable react/prop-types */
 const Test4 = (props) => {
-    return (
-      <div>
-        <div className="header">
-          <img className="logo" src="/src/image/LOGO.jpg" alt="LOGO" />
-          <div className="number">
-            <p>{props.tab}</p>
-          </div>
-          <ul className="navcart">
-            <li>
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </li>
-            <li>
-              <i className="fa-solid fa-cart-shopping"></i>
-            </li>
-          </ul>
+  const [list, setList] = useState(mockup);
+  const [listRef] = useState(list);
+  const [open, setOpen] = useState(false);
+  const [popUp, setPopUp] = useState();
+  const [selectStock, setSelectStock] = useState("all");
+  const [selectType, setSelectType] = useState("all");
+  return (
+    <div>
+      <div className="header">
+        <img className="logo" src="/src/image/LOGO.jpg" alt="LOGO" />
+        <div className="number">
+          <p>{props.tab}</p>
         </div>
-        <div className="Container"></div>
-        <dialog open={open}>
-          <div className="card">
-            <div className="navdialog">
-              <div className="navdialog">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className="searchnav"
-                />
-              </div>
-              <div>
-                <i className="fa-solid fa-circle-xmark"></i>
+        <ul className="navcart">
+          <li>
+            <i
+              className="fa-solid fa-magnifying-glass"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            ></i>
+          </li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+        </ul>
+      </div>
+      <div className="Container">
+        {popUp && (
+          <div className="warp-container">
+            <div className="listcontainercard">
+              <img className="listcontainerimg" src={popUp.img} alt="image" />
+              <div className="listcontainercardtext">
+                <div className="listcontainercardtextname">
+                  <p>{popUp.name}</p>
+                  <p>
+                    {Array.from({ length: 5 }).map((_, index) => {
+                      return index < popUp.score ? (
+                        <div key={index}>
+                          <i className="fa-solid fa-star"></i>
+                        </div>
+                      ) : (
+                        <div key={index}>
+                          <i className="fa-regular fa-star"></i>
+                        </div>
+                      );
+                    })}
+                  </p>
+                </div>
+                <div className="listcontainercardtextprice">
+                  <h2>{popUp.price}</h2>
+                </div>
               </div>
             </div>
-            <div className="listbox">
-              <div className="choice">
-                <div className="choicemenu">
-                  <button>Food</button>
-                  <button>Dessert</button>
-                  <button>Drink</button>
-                  <button>All</button>
-                </div>
-                <div className="choicesold">
-                  <button>In Stock</button>
-                  <button>Out Of Stock</button>
-                </div>
+          </div>
+        )}
+      </div>
+      <dialog open={open}>
+        <div className="card">
+          <div className="navdialog">
+            <div className="navdialog">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="searchnav"
+              />
+            </div>
+            <div>
+              <i
+                className="fa-solid fa-circle-xmark"
+                onClick={() => {
+                  setOpen(!open);
+                }}
+              ></i>
+            </div>
+          </div>
+          <div className="listbox">
+            <div className="choice">
+              <div className="choicemenu">
+                <button
+                  onClick={() => {
+                    const newlist = listRef.filter((item) => {
+                      const statusStock =
+                        item.stock > 0 ? "in stock" : "out of stock";
+                      return (
+                        item.type === "food" &&
+                        (statusStock === selectStock || selectStock === "all")
+                      );
+                    });
+                    setList(newlist);
+                    setSelectType("food");
+                  }}
+                >
+                  Food
+                </button>
+                <button
+                  onClick={() => {
+                    const newlist = listRef.filter((item) => {
+                      const statusStock =
+                        item.stock < 0 ? "in stock" : "out of stock";
+                      return (
+                        item.type === "dessert" &&
+                        (statusStock === selectStock || selectStock === "all")
+                      );
+                    });
+                    setList(newlist);
+                    setSelectType("dessert");
+                  }}
+                >
+                  Dessert
+                </button>
+                <button
+                  onClick={() => {
+                    const newlist = listRef.filter((item) => {
+                      const statusStock =
+                        item.stock > 0 ? "in stock" : "out of stock";
+                      return (
+                        item.type === "drink" &&
+                        (statusStock === selectStock || selectStock === "all")
+                      );
+                    });
+                    setList(newlist);
+                    setSelectType("drink");
+                  }}
+                >
+                  Drink
+                </button>
+                <button
+                  onClick={() => {
+                    const newlist = listRef.filter((item) => {
+                      const statusStock =
+                        item.stock > 0 ? "inStock" : "outStock";
+                      return (
+                        statusStock === selectStock || selectStock === "all"
+                      );
+                    });
+                    setList(newlist);
+                    setSelectType("all");
+                  }}
+                >
+                  All
+                </button>
               </div>
-              <div className="listdialog">
-                <div className="warp-listdialog">
-                  <div className="listdialogcard">
-                    <img className="listdialogimg" />
-                    <div className="listdialogcardtext">
-                      <div className="listdialogcardtextname">
-                        <p></p>
-                        <p></p>
-                      </div>
-                      <div className="istdialogcardtextprice">
-                        <h2></h2>
+              <div className="choicesold">
+                <button>In Stock</button>
+                <button>Out Of Stock</button>
+              </div>
+            </div>
+            <div className="listdialog">
+              {list.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="warp-listdialog"
+                    onClick={() => {
+                      setPopUp(item);
+                      setOpen(!open);
+                    }}
+                  >
+                    <div className="listdialogcard">
+                      <img
+                        className="listdialogimg"
+                        src={item.img}
+                        alt={item.name}
+                      />
+                      <div className="listdialogcardtext">
+                        <div className="listdialogcardtextname">
+                          <p>{item.name}</p>
+                          <p>
+                            {Array.from({ length: 5 }).map((_, index) => {
+                              return index < item.score ? (
+                                <div key={index}>
+                                  <i className="fa-solid fa-star"></i>
+                                </div>
+                              ) : (
+                                <div key={index}>
+                                  <i className="fa-regular fa-star"></i>
+                                </div>
+                              );
+                            })}
+                          </p>
+                        </div>
+                        <div className="istdialogcardtextprice">
+                          <h2>{item.price}</h2>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </div>
-        </dialog>
-      </div>
-    );
-  };
-  
-  export default Test4;
-  
+        </div>
+      </dialog>
+    </div>
+  );
+};
+
+export default Test4;
